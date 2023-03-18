95b2b633d9dab50ff93a0d46cc611fb90ca265ab
Ulisse Mini <ulisse.mini@gmail.com>
2023-03-18 17:15:18 -0500

    Adjustable date to fetch

--- main.py
+++ main.py
@@ -11,17 +11,14 @@ WEBHOOK_URL=os.environ['WEBHOOK_URL']
 OURA_API_KEY=os.environ['OURA_API_KEY']
 TTL = 60*60 # 1 hour
 
-TODAY = datetime.now().strftime('%Y-%m-%d')
-YESTERDAY = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-
 @st.cache_data(ttl=TTL)
-def get_toggl_day():
+def get_toggl_day(start_date: str, end_date: str):
     resp = httpx.get(
         'https://api.track.toggl.com/api/v9/me/time_entries',
         auth=(os.environ['TOGGL_API_KEY'], 'api_token'),
         params={
-            "start_date": YESTERDAY,
-            "end_date": TODAY,
+            "start_date": start_date,
+            "end_date": end_date,
         }
     )
     resp.raise_for_status()
@@ -37,12 +34,11 @@ def get_toggl_projects():
 
 
 @st.cache_data(ttl=TTL)
-def get_airtable_day():
+def get_airtable_day(date: str):
     resp = httpx.get(f'https://api.airtable.com/v0/appq1TYckaWsk1tgt/Daily', params={
         'maxRecords': 1,
         'view': 'Grid view',
-        'sort[0][field]': 'Date',
-        'sort[0][direction]': 'desc',
+        'filterByFormula': f'DATESTR(Date) = "{date}"',
     }, headers={'Authorization': f'Bearer {AIRTABLE_API_KEY}'})
     resp.raise_for_status()
     day = resp.json()["records"][0]["fields"]
@@ -50,30 +46,24 @@ def get_airtable_day():
 
 
 @st.cache_data(ttl=TTL)
-def get_oura_sleep():
+def get_oura_sleep(start_date: str, end_date: str):
     resp = httpx.get("https://api.ouraring.com/v2/usercollection/daily_sleep", params={
-        "start_date": TODAY,
-        "end_date": TODAY,
+        "start_date": start_date,
+        "end_date": end_date,
     }, headers={"Authorization": f"Bearer {OURA_API_KEY}"})
     resp.raise_for_status()
 
     return resp.json()
 
 
-def main():
-    st.title("Uli status")
-    st.write("A dashboard for Uli's daily status, how his life is going, etc.")
 
-
-    # TODO: Show past journals, slider for which day to look at
-    st.write("## Journal")
-    day = get_airtable_day()
-    msg = day['Journal'] if day['Date'] == YESTERDAY else "Uli didn't journal today! :("
-    st.write(msg)
-
-
+def show_oura_sleep(start_date: str, end_date: str):
     st.write("## Sleep")
-    sleep = get_oura_sleep()
+    sleep = get_oura_sleep(start_date, end_date)
+    if len(sleep['data']) == 0:
+        st.write("No sleep data found :(")
+        return
+
     oura_data = sleep['data'][0]
 
     # Sleep Score Gauge
@@ -81,15 +71,25 @@ def main():
         mode="gauge+number",
         value=oura_data["score"],
         domain={"x": [0, 1], "y": [0, 1]},
-        title={"text": "Sleep Score"},
+        title={"text": f"Sleep Score for {oura_data['day']}"},
         gauge={"axis": {"range": [0, 100], "tickwidth": 2}},
         number={"suffix": "/100"}
     ))
 
     st.plotly_chart(fig)
 
+
+def show_journal(date: str):
+    # TODO: Show past journals, slider for which day to look at
+    st.write("## Journal")
+    day = get_airtable_day(date)
+    msg = day['Journal'] if day['Date'] == date else "No journal data found :("
+    st.write(msg)
+
+
+def show_toggl_data(start_date: str, end_date: str):
     st.write("## Time tracking")
-    toggl = get_toggl_day()
+    toggl = get_toggl_day(start_date, end_date)
     toggl_projects = get_toggl_projects()
 
     df = pd.DataFrame(toggl)
@@ -102,6 +102,19 @@ def main():
     fig = px.pie(df[['project_name', 'duration']], values='duration', names='project_name')
     st.plotly_chart(fig)
 
+
+
+def main():
+    st.title("Uli status")
+    st.write("A dashboard for Uli's daily status, how his life is going, etc.")
+
+    # Use streamlit to get the date via a date picker
+    TODAY = st.date_input("Date", datetime.now()).strftime('%Y-%m-%d') # type: ignore
+    YESTERDAY = (datetime.strptime(TODAY, '%Y-%m-%d') - timedelta(days=1)).strftime('%Y-%m-%d')
+
+    show_journal(YESTERDAY)
+    show_oura_sleep(YESTERDAY, TODAY)
+    show_toggl_data(YESTERDAY, TODAY)
 
 
 if __name__ == '__main__':
